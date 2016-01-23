7f016afe5e91ee308fc2346c49bd35ec281a2533
Ken Haase <kh@beingmeta.com>
2016-01-23 14:33:04 -0500

    Make AES the default cipher

--- cryptofns.c
+++ cryptofns.c
@@ -82,7 +82,7 @@ U8_EXPORT ssize_t u8_cryptic
   ssize_t totalout=0, totalin=0;
   unsigned char inbuf[1024], outbuf[1024+EVP_MAX_BLOCK_LENGTH];
   const EVP_CIPHER *cipher=((cname)?(EVP_get_cipherbyname(cname)):
-			    (EVP_bf_cbc()));
+			    (EVP_aes_cbc()));
 
   if (cipher) {
     int needkeylen=EVP_CIPHER_key_length(cipher);
@@ -295,9 +295,9 @@ static u8_cipher ciphers[64]=
    NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
 static u8_string default_cipher_name="BLOWFISH";
 static struct U8_CCCIPHER default_cipher=
-  {"BLOWFISH",kCCAlgorithmBlowfish,kCCOptionPKCS7Padding,
-   kCCKeySizeMinBlowfish,kCCKeySizeMaxBlowfish,
-   kCCBlockSizeBlowfish,kCCBlockSizeBlowfish};
+  {"AES",kCCAlgorithmAES,kCCOptionPKCS7Padding,
+   kCCKeySizeAES128,kCCKeySizeAES256,
+   kCCBlockSizeAES128,kCCBlockSizeAES128};
 
 static struct U8_CCCIPHER *get_cipher(u8_string name)
 {
@@ -333,6 +333,9 @@ U8_EXPORT void u8_init_cryptofns_c()
   cryptofns_init=1;
 
   add_cc_cipher("AES",kCCAlgorithmAES,kCCOptionPKCS7Padding,
+		kCCKeySizeAES128,kCCKeySizeAES256,
+		kCCBlockSizeAES128,kCCBlockSizeAES128);
+  add_cc_cipher("AES128",kCCAlgorithmAES,kCCOptionPKCS7Padding,
 		kCCKeySizeAES128,kCCKeySizeAES256,
 		kCCBlockSizeAES128,kCCBlockSizeAES128);
   add_cc_cipher("3DES",kCCAlgorithm3DES,kCCOptionPKCS7Padding,
