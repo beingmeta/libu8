d67cbb73fb13b2c9a58297170d916ca8399b8811
Ken Haase <kh@beingmeta.com>
2016-02-13 17:03:30 -0500

    Started adding support for RSA

--- cryptofns.c
+++ cryptofns.c
@@ -28,6 +28,7 @@
      (!((HAVE_COMMONCRYPTO_COMMONCRYPTOR_H)&&(HAVE_CCCRYPTORCREATE))))
 #include <openssl/err.h>
 #include <openssl/evp.h>
+#include <openssl/rsa.h>
 #elif ((HAVE_COMMONCRYPTO_COMMONCRYPTOR_H)&&(HAVE_CCCRYPTORCREATE))
 #include <CommonCrypto/CommonCryptor.h>
 #else
@@ -63,6 +64,13 @@ U8_EXPORT unsigned char *u8_random_vector(int len)
   else return vec;
 }
 
+static int fill_bytebuf(U8_BYTEBUF *out,u8_block_reader reader,void *readstate)
+{
+  unsigned char buf[256]; ssize_t n_bytes;
+  while ((n_bytes=reader(buf,256,readstate))>0) 
+    u8_bufwrite(out,buf,n_bytes);
+}
+
 #if ((HAVE_EVP_CIPHER_CTX_INIT)&&(HAVE_OPENSSL_EVP_H)&& \
      (HAVE_OPENSSL_ERR_H)&&(HAVE_OPENSSL_EVP_H)&& \
      (!((HAVE_COMMONCRYPTO_COMMONCRYPTOR_H)&&(HAVE_CCCRYPTORCREATE))))
@@ -77,107 +85,147 @@ U8_EXPORT ssize_t u8_cryptic
  void *readstate,void *writestate,
  u8_context caller)
 {
-  EVP_CIPHER_CTX ctx;
-  int inlen, outlen, retval=0;
-  ssize_t totalout=0, totalin=0;
-  unsigned char inbuf[1024], outbuf[1024+EVP_MAX_BLOCK_LENGTH];
-  const EVP_CIPHER *cipher=((cname)?(EVP_get_cipherbyname(cname)):
-			    (EVP_aes_128_cbc()));
+  if (strncasecmp(cname,"rsa",3)==0) {
+#if 0
+    EVP_PKEY _pkey, *pkey; EVP_PKEY_CTX *ctx; ENGINE *eng;
+    int padding=RSA_PKCS1_OAEP_PADDING;
+    int pubkey=(strncasecmp(cname,"rsapub",6)==0);
+    const unsigned char *scankey=key;
+    struct U8_BYTEBUF bb;
+    int retval;
+    if (pubkey)
+      pkey=d2i_PublicKey(EVP_PKEY_RSA,NULL,&scankey,keylen);
+    else pkey=d2i_PrivateKey(EVP_PKEY_RSA,NULL,&scankey,keylen);
+    bb.u8_direction=u8_output_buffer;
+    bb.u8_buf=bb.u8_ptr=(u8_byte *)u8_malloc(1024);
+    bb.u8_lim=(u8_byte *)(bb.u8_buf+1024);
+    bb.u8_growbuf=1;
+    fill_bytebuf(&bb,reader,readstate);
+    ctx=EVP_PKEY_CTX_new(pkey,eng);
+    if (!(ctx)) {}
+    else if ((EVP_PKEY_encrypt_init(ctx)<0)||
+	     (EVP_PKEY_CTX_set_rsa_padding(ctx,padding)<0)) {}
+    else {
+      unsigned char *in=bb.u8_buf; size_t inlen=bb.u8_ptr-bb.u8_buf;
+      unsigned char *out; size_t outlen;
+      if (do_encrypt) retval=EVP_PKEY_encrypt(ctx,NULL,&outlen,in,inlen);
+      else retval=retval=EVP_PKEY_decrypt(ctx,NULL,&outlen,in,inlen);
+      if (retval<0) {}
+      else if ((out=OPENSSL_malloc(outlen))==NULL) {}
+      else if (do_encrypt)
+	retval=EVP_PKEY_encrypt(ctx,out,&outlen,in,inlen);
+      else retval=EVP_PKEY_decrypt(ctx,out,&outlen,in,inlen);
+      if (retval<0) {}
+      else retval=writer(out,outlen,writestate);
+      return retval;}
+    #else
+    u8_seterr(_("RSA NYI"),"u8_cryptic",u8_strdup(cname));
+    return -1;
+    #endif
+  }
+  else {
+    EVP_CIPHER_CTX ctx;
+    int inlen, outlen, retval=0;
+    ssize_t totalout=0, totalin=0;
+    unsigned char inbuf[1024], outbuf[1024+EVP_MAX_BLOCK_LENGTH];
+    const EVP_CIPHER *cipher=((cname)?(EVP_get_cipherbyname(cname)):
+			      (EVP_aes_128_cbc()));
 
-  if (cipher) {
-    int needkeylen=EVP_CIPHER_key_length(cipher);
-    int needivlen=EVP_CIPHER_iv_length(cipher);
-    int blocksize=EVP_CIPHER_block_size(cipher);
-    if (blocksize>1024) blocksize=1024;
-    u8_log(CRYPTO_LOGLEVEL,OPENSSL_CRYPTIC,
-	   " %s cipher=%s, keylen=%d/%d, ivlen=%d/%d, blocksize=%d\n",
-	   ((do_encrypt)?("encrypt"):("decrypt")),
-	   cname,keylen,needkeylen,ivlen,needivlen,blocksize);
+    if (cipher) {
+      int needkeylen=EVP_CIPHER_key_length(cipher);
+      int needivlen=EVP_CIPHER_iv_length(cipher);
+      int blocksize=EVP_CIPHER_block_size(cipher);
+      if (blocksize>1024) blocksize=1024;
+      u8_log(CRYPTO_LOGLEVEL,OPENSSL_CRYPTIC,
+	     " %s cipher=%s, keylen=%d/%d, ivlen=%d/%d, blocksize=%d\n",
+	     ((do_encrypt)?("encrypt"):("decrypt")),
+	     cname,keylen,needkeylen,ivlen,needivlen,blocksize);
 
-    if ((needivlen)&&(ivlen)&&(ivlen!=needivlen))
-      return u8_reterr(u8_BadCryptoIV,
-		       ((caller)?(caller):(OPENSSL_CRYPTIC)),
-		       u8_mkstring("%d!=%d(%s)",ivlen,needivlen,cname));
+      if ((needivlen)&&(ivlen)&&(ivlen!=needivlen))
+	return u8_reterr(u8_BadCryptoIV,
+			 ((caller)?(caller):(OPENSSL_CRYPTIC)),
+			 u8_mkstring("%d!=%d(%s)",ivlen,needivlen,cname));
 
-    memset(&ctx,0,sizeof(ctx));
+      memset(&ctx,0,sizeof(ctx));
 
-    EVP_CIPHER_CTX_init(&ctx);
+      EVP_CIPHER_CTX_init(&ctx);
 
-    retval=EVP_CipherInit(&ctx, cipher, NULL, NULL, do_encrypt);
-    if (retval==0)
-      return u8_reterr(u8_CipherInit_Failed,
-		       ((caller)?(caller):(OPENSSL_CRYPTIC)),
-		       u8_strdup(cname));
+      retval=EVP_CipherInit(&ctx, cipher, NULL, NULL, do_encrypt);
+      if (retval==0)
+	return u8_reterr(u8_CipherInit_Failed,
+			 ((caller)?(caller):(OPENSSL_CRYPTIC)),
+			 u8_strdup(cname));
 
-    retval=EVP_CIPHER_CTX_set_key_length(&ctx,keylen);
-    if (retval==0)
-      return u8_reterr(u8_BadCryptoKey,
-		       ((caller)?(caller):(OPENSSL_CRYPTIC)),
-		       u8_mkstring("%d!=%d(%s)",keylen,needkeylen,cname));
+      retval=EVP_CIPHER_CTX_set_key_length(&ctx,keylen);
+      if (retval==0)
+	return u8_reterr(u8_BadCryptoKey,
+			 ((caller)?(caller):(OPENSSL_CRYPTIC)),
+			 u8_mkstring("%d!=%d(%s)",keylen,needkeylen,cname));
 
-    if ((needivlen)&&(ivlen!=needivlen))
-      return u8_reterr(u8_BadCryptoIV,
-		       ((caller)?(caller):(OPENSSL_CRYPTIC)),
-		       u8_mkstring("%d!=%d(%s)",ivlen,needivlen,cname));
+      if ((needivlen)&&(ivlen!=needivlen))
+	return u8_reterr(u8_BadCryptoIV,
+			 ((caller)?(caller):(OPENSSL_CRYPTIC)),
+			 u8_mkstring("%d!=%d(%s)",ivlen,needivlen,cname));
 
-    retval=EVP_CipherInit(&ctx, cipher, key, iv, do_encrypt);
-    if (retval==0)
-      return u8_reterr(u8_CipherInit_Failed,
-		       ((caller)?(caller):(OPENSSL_CRYPTIC)),
-		       u8_strdup(cname));
+      retval=EVP_CipherInit(&ctx, cipher, key, iv, do_encrypt);
+      if (retval==0)
+	return u8_reterr(u8_CipherInit_Failed,
+			 ((caller)?(caller):(OPENSSL_CRYPTIC)),
+			 u8_strdup(cname));
 
-    while (1) {
-      inlen = reader(inbuf,blocksize,readstate);
-      if (inlen <= 0) {
-	u8_log(CRYPTO_LOGLEVEL,OPENSSL_CRYPTIC,
-	       "Finished %s(%s) with %ld in, %ld out",
-	       ((do_encrypt)?("encrypt"):("decrypt")),
-	       cname,totalin,totalout);
-	break;}
-      else totalin=totalin+inlen;
-      if (!(EVP_CipherUpdate(&ctx,outbuf,&outlen,inbuf,inlen))) {
+      while (1) {
+	inlen = reader(inbuf,blocksize,readstate);
+	if (inlen <= 0) {
+	  u8_log(CRYPTO_LOGLEVEL,OPENSSL_CRYPTIC,
+		 "Finished %s(%s) with %ld in, %ld out",
+		 ((do_encrypt)?("encrypt"):("decrypt")),
+		 cname,totalin,totalout);
+	  break;}
+	else totalin=totalin+inlen;
+	if (!(EVP_CipherUpdate(&ctx,outbuf,&outlen,inbuf,inlen))) {
+	  char *details=u8_malloc(256);
+	  unsigned long err=ERR_get_error();
+	  ERR_error_string_n(err,details,256);
+	  EVP_CIPHER_CTX_cleanup(&ctx);
+	  return u8_reterr(u8_InternalCryptoError,
+			   ((caller)?(caller):((u8_context)"u8_cryptic")),
+			   details);}
+	else {
+	  u8_log(CRYPTO_LOGLEVEL,OPENSSL_CRYPTIC,
+		 "%s(%s) consumed %d/%ld bytes, emitted %d/%ld bytes"
+		 " in=<%v>\n out=<%v>",
+		 ((do_encrypt)?("encrypt"):("decrypt")),cname,
+		 inlen,totalin,outlen,totalout+outlen,
+		 inbuf,inlen,outbuf,outlen);
+	  writer(outbuf,outlen,writestate);
+	  totalout=totalout+outlen;}}
+      if (!(EVP_CipherFinal(&ctx,outbuf,&outlen))) {
 	char *details=u8_malloc(256);
 	unsigned long err=ERR_get_error();
 	ERR_error_string_n(err,details,256);
 	EVP_CIPHER_CTX_cleanup(&ctx);
 	return u8_reterr(u8_InternalCryptoError,
-			 ((caller)?(caller):((u8_context)"u8_cryptic")),
+			 ((caller)?(caller):(OPENSSL_CRYPTIC)),
 			 details);}
       else {
+	writer(outbuf,outlen,writestate);
 	u8_log(CRYPTO_LOGLEVEL,OPENSSL_CRYPTIC,
-	       "%s(%s) consumed %d/%ld bytes, emitted %d/%ld bytes"
-	       " in=<%v>\n out=<%v>",
+	       "%s(%s) done after consuming %ld/%ld bytes, emitting %ld/%ld bytes"
+	       "\n final out=<%v>",
 	       ((do_encrypt)?("encrypt"):("decrypt")),cname,
 	       inlen,totalin,outlen,totalout+outlen,
-	       inbuf,inlen,outbuf,outlen);
-	writer(outbuf,outlen,writestate);
-	totalout=totalout+outlen;}}
-    if (!(EVP_CipherFinal(&ctx,outbuf,&outlen))) {
+	       outbuf,outlen);
+	EVP_CIPHER_CTX_cleanup(&ctx);
+	totalout=totalout+outlen;
+	return totalout;}}
+    else {
       char *details=u8_malloc(256);
       unsigned long err=ERR_get_error();
       ERR_error_string_n(err,details,256);
-      EVP_CIPHER_CTX_cleanup(&ctx);
-      return u8_reterr(u8_InternalCryptoError,
-		       ((caller)?(caller):(OPENSSL_CRYPTIC)),
+      return u8_reterr("Unknown cipher",
+		       ((caller)?(caller):((u8_context)"u8_cryptic")),
 		       details);}
-    else {
-      writer(outbuf,outlen,writestate);
-      u8_log(CRYPTO_LOGLEVEL,OPENSSL_CRYPTIC,
-	     "%s(%s) done after consuming %ld/%ld bytes, emitting %ld/%ld bytes"
-	     "\n final out=<%v>",
-	     ((do_encrypt)?("encrypt"):("decrypt")),cname,
-	     inlen,totalin,outlen,totalout+outlen,
-	     outbuf,outlen);
-      EVP_CIPHER_CTX_cleanup(&ctx);
-      totalout=totalout+outlen;
-      return totalout;}}
-  else {
-    char *details=u8_malloc(256);
-    unsigned long err=ERR_get_error();
-    ERR_error_string_n(err,details,256);
-    return u8_reterr("Unknown cipher",
-		     ((caller)?(caller):((u8_context)"u8_cryptic")),
-		     details);}
+  }
 }
 
 U8_EXPORT void u8_init_cryptofns_c()
@@ -209,82 +257,87 @@ U8_EXPORT ssize_t u8_cryptic
  void *readstate,void *writestate,
  u8_context caller)
 {
-  CCCryptorRef ctx;
-  CCOptions options=0;
-  ssize_t inlen, outlen, totalin=0, totalout=0, retval=0;
-  unsigned char inbuf[1024], outbuf[1024];
-  struct U8_CCCIPHER *cipher=get_cipher(cname);
-  if (cipher) {
-    size_t blocksize=cipher->cc_blocksize;
-    ssize_t needivlen=cipher->cc_ivlen;
-    if (!((keylen<=cipher->cc_keymax)&&(keylen>=cipher->cc_keymin)))
-      return u8_reterr(u8_BadCryptoKey,
-		       ((caller)?(caller):((u8_context)"u8_cryptic")),
-		       u8_mkstring("%d!=[%d,%d](%s)",keylen,
-				   cipher->cc_keymin,cipher->cc_keymax,
-				   cname));
-    if ((needivlen)&&(ivlen!=needivlen))
-      return u8_reterr(u8_BadCryptoIV,
-		       ((caller)?(caller):(COMMONCRYPTO_CRYPTIC)),
-		       u8_mkstring("%d!=%d(%s)",ivlen,needivlen,cname));
+  if (strncasecmp(cname,"rsa",3)==0) {
+    u8_seterr(_("RSA support NYI"),"u8_cryptic",u8_strdup(cname));
+    return -1;}
+  else {
+    CCCryptorRef ctx;
+    CCOptions options=0;
+    ssize_t inlen, outlen, totalin=0, totalout=0, retval=0;
+    unsigned char inbuf[1024], outbuf[1024];
+    struct U8_CCCIPHER *cipher=get_cipher(cname);
+    if (cipher) {
+      size_t blocksize=cipher->cc_blocksize;
+      ssize_t needivlen=cipher->cc_ivlen;
+      if (!((keylen<=cipher->cc_keymax)&&(keylen>=cipher->cc_keymin)))
+	return u8_reterr(u8_BadCryptoKey,
+			 ((caller)?(caller):((u8_context)"u8_cryptic")),
+			 u8_mkstring("%d!=[%d,%d](%s)",keylen,
+				     cipher->cc_keymin,cipher->cc_keymax,
+				     cname));
+      if ((needivlen)&&(ivlen!=needivlen))
+	return u8_reterr(u8_BadCryptoIV,
+			 ((caller)?(caller):(COMMONCRYPTO_CRYPTIC)),
+			 u8_mkstring("%d!=%d(%s)",ivlen,needivlen,cname));
 
-    if (needivlen==0) iv=NULL;
+      if (needivlen==0) iv=NULL;
 
-    memset(&ctx,0,sizeof(ctx));
+      memset(&ctx,0,sizeof(ctx));
 
-    CCCryptorStatus status=CCCryptorCreate
-      (((do_encrypt)? (kCCEncrypt) : (kCCDecrypt)),
-       cipher->cc_algorithm,cipher->cc_opts,key,keylen,iv,&ctx);
+      CCCryptorStatus status=CCCryptorCreate
+	(((do_encrypt)? (kCCEncrypt) : (kCCDecrypt)),
+	 cipher->cc_algorithm,cipher->cc_opts,key,keylen,iv,&ctx);
 
-    u8_log(CRYPTO_LOGLEVEL,COMMONCRYPTO_CRYPTIC,
-	   " %s cipher=%s, keylen=%d/[%d,%d], ivlen=%d, blocksize=%d\n",
-	   ((do_encrypt)?("encrypt"):("decrypt")),
-	   cname,keylen,cipher->cc_keymin,cipher->cc_keymax,
-	   ivlen,blocksize);
+      u8_log(CRYPTO_LOGLEVEL,COMMONCRYPTO_CRYPTIC,
+	     " %s cipher=%s, keylen=%d/[%d,%d], ivlen=%d, blocksize=%d\n",
+	     ((do_encrypt)?("encrypt"):("decrypt")),
+	     cname,keylen,cipher->cc_keymin,cipher->cc_keymax,
+	     ivlen,blocksize);
 
-    while (1) {
-      inlen = reader(inbuf,blocksize,readstate);
-      if (inlen <= 0) {
-	u8_log(CRYPTO_LOGLEVEL,COMMONCRYPTO_CRYPTIC,
-	       "Finished %s(%s) with %ld in, %ld out",
-	       ((do_encrypt)?("encrypt"):("decrypt")),cname,
-	       totalin,totalout);
-	break;}
-      if ((status=CCCryptorUpdate(ctx,inbuf,inlen,outbuf,1024,&outlen))
-	  !=kCCSuccess) {
+      while (1) {
+	inlen = reader(inbuf,blocksize,readstate);
+	if (inlen <= 0) {
+	  u8_log(CRYPTO_LOGLEVEL,COMMONCRYPTO_CRYPTIC,
+		 "Finished %s(%s) with %ld in, %ld out",
+		 ((do_encrypt)?("encrypt"):("decrypt")),cname,
+		 totalin,totalout);
+	  break;}
+	if ((status=CCCryptorUpdate(ctx,inbuf,inlen,outbuf,1024,&outlen))
+	    !=kCCSuccess) {
+	  CCCryptorRelease(ctx);
+	  return u8_reterr(u8_InternalCryptoError,
+			   ((caller)?(caller):((u8_context)"u8_cryptic")),
+			   NULL);}
+	else {
+	  u8_log(CRYPTO_LOGLEVEL,COMMONCRYPTO_CRYPTIC,
+		 "%s(%s) consumed %d/%ld bytes, emitted %d/%ld bytes"
+		 " in=<%v>\n out=<%v>",
+		 ((do_encrypt)?("encrypt"):("decrypt")),cname,
+		 inlen,totalin,outlen,totalout+outlen,
+		 inbuf,inlen,outbuf,outlen);
+	  writer(outbuf,outlen,writestate);
+	  totalout=totalout+outlen;}}
+
+      if ((status=CCCryptorFinal(ctx,outbuf,1024,&outlen))!=kCCSuccess) {
 	CCCryptorRelease(ctx);
 	return u8_reterr(u8_InternalCryptoError,
 			 ((caller)?(caller):((u8_context)"u8_cryptic")),
 			 NULL);}
       else {
+	writer(outbuf,outlen,writestate);
 	u8_log(CRYPTO_LOGLEVEL,COMMONCRYPTO_CRYPTIC,
-	       "%s(%s) consumed %d/%ld bytes, emitted %d/%ld bytes"
-	       " in=<%v>\n out=<%v>",
+	       "%s(%s) done after consuming %ld/%ld bytes, emitting %ld/%ld bytes"
+	       "\n final out=<%v>",
 	       ((do_encrypt)?("encrypt"):("decrypt")),cname,
 	       inlen,totalin,outlen,totalout+outlen,
-	       inbuf,inlen,outbuf,outlen);
-	writer(outbuf,outlen,writestate);
-	totalout=totalout+outlen;}}
-
-    if ((status=CCCryptorFinal(ctx,outbuf,1024,&outlen))!=kCCSuccess) {
-      CCCryptorRelease(ctx);
-      return u8_reterr(u8_InternalCryptoError,
-		       ((caller)?(caller):((u8_context)"u8_cryptic")),
-		       NULL);}
-    else {
-      writer(outbuf,outlen,writestate);
-      u8_log(CRYPTO_LOGLEVEL,COMMONCRYPTO_CRYPTIC,
-	     "%s(%s) done after consuming %ld/%ld bytes, emitting %ld/%ld bytes"
-	     "\n final out=<%v>",
-	     ((do_encrypt)?("encrypt"):("decrypt")),cname,
-	     inlen,totalin,outlen,totalout+outlen,
-	     outbuf,outlen);
-      CCCryptorRelease(ctx);
-      totalout=totalout+outlen;
-      return totalout;}}
-  else return u8_reterr("Unknown cipher",
-			((caller)?(caller):((u8_context)"u8_cryptic")),
-			u8_strdup(cname));
+	       outbuf,outlen);
+	CCCryptorRelease(ctx);
+	totalout=totalout+outlen;
+	return totalout;}}
+    else return u8_reterr("Unknown cipher",
+			  ((caller)?(caller):((u8_context)"u8_cryptic")),
+			  u8_strdup(cname));
+  }
 }
 
 #define MAX_CIPHERS 64
