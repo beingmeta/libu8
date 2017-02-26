548ea31bbfa6ad9c51fbf696ed980495771df1d1
Ken Haase <kh@beingmeta.com>
2017-02-26 15:39:32 -0500

    Add U8_INT2PTR

--- include/libu8/u8defines.h
+++ include/libu8/u8defines.h
@@ -201,6 +201,7 @@ typedef unsigned int u8_wideint;
 #endif
 
 #define U8_PTR2INT(x) ((u8_wideint)(x))
+#define U8_INT2PTR(x) ((void *)((u8_wideint)(x)))
 
 #ifndef WORDS_BIGENDIAN
 #define WORDS_BIGENDIAN 0
