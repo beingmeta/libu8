4b8a37b26d9c846f69887fcfb145549ad845b34e
Ken Haase <kh@beingmeta.com>
2017-10-31 08:28:55 -0400

    Fixes to signal handling in u8run

--- u8run.c
+++ u8run.c
@@ -32,20 +32,23 @@
 
 static u8_string rundir = "/var/run/u8run/";
 
+static u8_string main_job_id = NULL;
 static u8_string pid_file = NULL;
 static u8_string ppid_file = NULL;
+static pid_t dependent = -1;
+
 
 void usage()
 {
   fprintf(stderr,"fdrun [+daemon] [env=val]* jobid [env=val]* exename [args..]\n");
 }
 
-static u8_string procpath(u8_string jobid,u8_string suffix)
+static u8_string procpath(u8_string job_id,u8_string suffix)
 {
-  if (strchr(jobid,'/'))
-    return u8_string_append(jobid,".",suffix,NULL);
+  if (strchr(job_id,'/'))
+    return u8_string_append(job_id,".",suffix,NULL);
   else {
-    u8_string name = u8_string_append(jobid,".",suffix,NULL);
+    u8_string name = u8_string_append(job_id,".",suffix,NULL);
     u8_string path = u8_mkpath(rundir,name);
     u8_free(name);
     return path;}
@@ -55,7 +58,7 @@ static int n_cycles=0, doexit=0, paused=0, restart=0;
 static double last_launch = -1, fast_fail = 3, fail_start=-1;
 static double exit_wait=0, error_wait=1.0, max_wait=120, backoff=10;
 
-static int launch_loop(u8_string jobid,char **real_args,int n_args);
+static int launch_loop(u8_string job_id,char **real_args,int n_args);
 static void setup_signals(void);
 
 int main(int argc,char *argv[])
@@ -101,7 +104,9 @@ int main(int argc,char *argv[])
       memcpy(&(launch_args[0]),&(argv[i]),sizeof(char *)*n_args);
       break;}}
   launch_args[n_args]=NULL;
+  main_job_id = job_id;
 
+  if (getenv("LOGLEVEL")) u8_loglevel=u8_getenv_int("LOGLEVEL",5);
   if (getenv("RUNDIR"))   rundir=u8_getenv("RUNDIR");
   if (getenv("FASTFAIL")) fast_fail=u8_getenv_float("FASTFAIL",fast_fail);
   if (getenv("EXITWAIT")) exit_wait=u8_getenv_float("EXITWAIT",exit_wait);
@@ -117,11 +122,11 @@ int main(int argc,char *argv[])
   if (launching) {
     pid_t launched = fork();
     if (launched<0) {
-      u8_log(LOGCRIT,"ForkFailed",
+      u8_log(LOG_CRIT,"ForkFailed",
 	     "Couldn't fork to launch %s",job_id);
       exit(1);}
     else if (launched) {
-      u8_log(LOGNOTICE,"u8run/launch",
+      u8_log(LOG_NOTICE,"u8run/launch",
 	     "Forked (%lld) launch loop for %s (%s), waiting for %s",
 	     (long long)launched,job_id,cmd,pid_file);
       while (! (u8_file_existsp(pid_file)) ) sleep(1);
@@ -132,15 +137,19 @@ int main(int argc,char *argv[])
   return launch_loop(job_id,launch_args,n_args);
 }
 
-static pid_t kill_child(pid_t pid,u8_string pid_file)
+static pid_t kill_child(u8_string job_id,pid_t pid,u8_string pid_file)
 {
   if (pid<=0) return -1;
+  u8_log(LOG_INFO,"u8run/signal","Sending SIGTERM to %s:%lld",job_id,pid);
   int rv = kill(pid,SIGTERM);
   if (rv<0) return rv;
   long long term_wait = u8_getenv_int("TERMWAIT",30);
   double term_pause = u8_getenv_float("TERMPAUSE",0.4);
   double checkin = u8_elapsed_time();
   while (u8_file_existsp(pid_file)) {
+    u8_log(LOG_INFO,"u8run/signal/wait",
+	   "Waiting for PID file %s to be removed by %s:%lld",
+	   pid_file,job_id,pid);
     double now = u8_elapsed_time();
     if ( (now-checkin) > term_wait) {
       int rv = kill(pid,SIGKILL);
@@ -148,25 +157,30 @@ static pid_t kill_child(pid_t pid,u8_string pid_file)
       else u8_removefile(pid_file);
       return 0;}
     else u8_sleep(term_pause);}
+  u8_log(LOG_NOTICE,"u8run/signal/wait",
+	 "%s:%lld finished, PID file %s gone",
+	 pid_file,job_id,pid);
   return -1;
 }
 
 static pid_t dolaunch(char **launch_args)
 {
   if (u8_file_existsp(pid_file)) {
-    u8_log(LOGWARN,"LeftoverPIDfile",
+    u8_log(LOG_WARN,"LeftoverPIDfile",
 	   "Removing leftover pid file %s",pid_file);
     int rv = u8_removefile(pid_file);
     if (rv<0) {
-      u8_log(LOGCRIT,"PIDCleanupFailed",
+      u8_log(LOG_CRIT,"PIDCleanupFailed",
 	     "Couldn't remove existing PID file %s",pid_file);
       return -1;}}
 
   double now = u8_elapsed_time();
   last_launch = u8_elapsed_time();
   pid_t pid = fork();
-  if (pid == 0)
-    return execvp(launch_args[0],launch_args);
+  if (pid == 0) {
+    signal(SIGINT,SIG_IGN);
+    signal(SIGTSTP,SIG_IGN);
+    return execvp(launch_args[0],launch_args);}
   else return pid;
 }
 
@@ -178,8 +192,9 @@ static void fail_wait(u8_condition reason,u8_string job_id,char **launch_args)
   else fail_duration = u8_elapsed_time()-fail_start;
   double fail_pause = (fail_duration > max_wait) ? (fail_duration) :
     fail_duration+backoff;
-  u8_log(LOGWARN,reason,"Pausing job %s (%s) for %f seconds",
-	 job_id,launch_args[0],fail_pause);
+  u8_log(LOG_NOTICE,reason,
+	 "Waiting %f seconds before resuming %s (%s)",
+	 fail_pause,job_id,launch_args[0]);
   u8_sleep(fail_pause);
 }
 
@@ -188,19 +203,28 @@ static void fail_wait(u8_condition reason,u8_string job_id,char **launch_args)
 struct sigaction sigaction_pause, sigaction_cont;
 struct sigaction sigaction_exit, sigaction_hup;
 
+static void log_signal(int signum,siginfo_t *info,void *stuff)
+{
+  u8_log(LOG_INFO,"Signal","Signal %d, errno=%d, status=%d",
+	 info->si_signo,info->si_errno,info->si_status);
+}
+
 static void siginfo_cont(int signum,siginfo_t *info,void *stuff)
 {
-  paused = 0; restart = 1;
+  paused = 0;
+  restart = 1;
 }
 
 static void siginfo_exit(int signum,siginfo_t *info,void *stuff)
 {
-  paused = 0; doexit = 1;
+  paused = 0;
+  doexit = 1;
 }
 
 static void siginfo_hup(int signum,siginfo_t *info,void *stuff)
 {
-  paused = 0; restart = 1;
+  paused = 0;
+  restart = 1;
 }
 
 static void siginfo_pause(int signum,siginfo_t *info,void *stuff)
@@ -226,15 +250,19 @@ static void setup_signals()
   sigaction_hup.sa_flags = SA_SIGINFO;
   sigemptyset(&(sigaction_hup.sa_mask));
 
+  sigaction(SIGINT,&(sigaction_exit),NULL);
   sigaction(SIGTERM,&(sigaction_exit),NULL);
   sigaction(SIGQUIT,&(sigaction_exit),NULL);
+  sigaction(SIGTSTP,&(sigaction_pause),NULL);
   sigaction(SIGUSR1,&(sigaction_pause),NULL);
   sigaction(SIGCONT,&(sigaction_cont),NULL);
   sigaction(SIGHUP,&(sigaction_hup),NULL);
 }
 
-static void remove_ppid_file()
+static void exit_u8run()
 {
+  if (dependent > 0)
+    kill_child(main_job_id,dependent,pid_file);
   if (ppid_file) {
     u8_string filename = ppid_file;
     int rv = u8_removefile(filename);
@@ -253,8 +281,7 @@ static u8_string write_ppid_file(u8_string job_id)
     pid_t pid = getpid();
     u8_byte pidstring[64]; u8_sprintf(pidstring,64,"%lld",pid);
     fputs(pidstring,f);
-    fclose(f);
-    atexit(remove_ppid_file);}
+    fclose(f);}
   else {
     u8_log(LOGWARN,"PPIDFile","Couldn't write PPID file %s",ppid_file);
     return NULL;}
@@ -265,6 +292,8 @@ static u8_string write_ppid_file(u8_string job_id)
 static int launch_loop(u8_string job_id,char **launch_args,int n_args)
 {
   write_ppid_file(job_id);
+
+  atexit(exit_u8run);
 
   u8_log(LOGNOTICE,"u8run","Sustaining job %s %s",launch_args[0],job_id);
   {int i=1; while (i < n_args) {
@@ -280,6 +309,10 @@ static int launch_loop(u8_string job_id,char **launch_args,int n_args)
     int exited = (wait_rv == pid) && (kill(pid,0) < 0);
     double now = u8_elapsed_time();
 
+    u8_log(LOGNOTICE,"u8run/INT",
+	   "Job %s:%lld signalled with rv=%d, do_exit=%d, paused=%d, restart=%d",
+	   job_id,pid,wait_rv,doexit,paused,restart);
+
     if (exited)
       u8_log(LOGNOTICE,"u8run","Job %s:%lld exited %s with status %d",
 	     job_id,pid,
@@ -288,7 +321,7 @@ static int launch_loop(u8_string job_id,char **launch_args,int n_args)
 
     if (doexit) {
       /* If someone has set doexit, just terminate the child and exit */
-      if (! exited) pid = kill_child(pid,pid_file);
+      if (! exited) pid = kill_child(job_id,pid,pid_file);
       exit(0);}
     else if ( (exited) && ((now-started) < fast_fail) )
       /* If the child exited very quickly, count it as a failure
@@ -305,12 +338,12 @@ static int launch_loop(u8_string job_id,char **launch_args,int n_args)
       u8_log(LOG_WARN,"Paused","Pausing %s, terminating %lld",
 	     job_id,(long long)pid);
       if ( (pid>0) && (!(exited)) )
-	pid = kill_child(pid,pid_file);
+	pid = kill_child(job_id,pid,pid_file);
       while ( (paused) && (pause_rv=wait(&pause_status)) ) {
 	u8_log(LOG_INFO,"WakeUp","Woke up for rv=%d",pause_rv);
 	/* Handle signals while paused */
 	if (doexit) {
-	  kill_child(pid,pid_file);
+	  kill_child(job_id,pid,pid_file);
 	  exit(0);}
 	else if (restart) {
 	  paused=0; break;}
@@ -320,14 +353,14 @@ static int launch_loop(u8_string job_id,char **launch_args,int n_args)
 	else break;}}
     if (doexit) {
       if (! exited )
-	pid = kill_child(pid,pid_file);
+	pid = kill_child(job_id,pid,pid_file);
       exit(0);}
     else if (restart) {
       u8_log(LOG_NOTICE,"RESTART","Handling requested restart of %s",job_id);
       if ( (! exited ) && (pid>0) ) {
 	u8_log(LOG_WARN,"Restart/cleanup",
 	       "Terminating existing %s:%lld",job_id,pid);
-	pid = kill_child(pid,pid_file);}
+	pid = kill_child(job_id,pid,pid_file);}
       else u8_log(LOG_INFO,"Restarting","Restarting job %s",job_id);
       pid = dolaunch(launch_args);
       u8_log(LOG_NOTICE,"Restarted","Restarted job %s:%lld",job_id,pid);
